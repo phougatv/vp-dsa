820fc47b439449b1b4038163e1edce1d97e3e50b
Vedant Phougat <vedant.phougat@hotmail.com>
2021-09-26 17:21:38 +0530

    Added Algorithm project, InsertionSort and its UTCs.

---
+++ src/Algorithms/VP.DSA.Algorithms/Extension.cs
@@ -0,0 +1,22 @@
+﻿namespace VP.DSA.Algorithms;
+public static class Extension
+{
+    /// <summary>
+    /// Swaps the elements of array at indices i and j.
+    /// </summary>
+    /// <param name="array">The array.</param>
+    /// <param name="i">The index i.</param>
+    /// <param name="j">The index j.</param>
+    public static void Swap(this Int32[] array, Int32 i, Int32 j)
+    {
+        array[i] = array[i] + array[j];
+        array[j] = array[i] - array[j];
+        array[i] = array[i] - array[j];
+    }
+
+    #region Generic Exntesion Methods
+    public static Boolean HasOnlyOneItem<T>(this T[] array) => array.Length == 1;
+    public static Boolean IsEmpty<T>(this T[] array) => array.Length == 0;
+    public static Boolean IsNull<T>(this T[] array) where T : class => array.Length == 0;
+    #endregion Generic Exntesion Methods
+}
---
+++ src/Algorithms/VP.DSA.Algorithms/Sort/InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿namespace VP.DSA.Algorithms.Sort;
+public class InsertionSort
+{
+    public Int32[] Sort(Int32[] array)
+    {
+        if (array.IsEmpty())
+            return array;
+        if (array.HasOnlyOneItem())
+            return array;
+
+        return ProtectedSort(array);
+    }
+
+    protected Int32[] ProtectedSort(Int32[] array)
+    {
+        var i = 1;
+        Int32 j;
+        while (i < array.Length)
+        {
+            j = i;
+            while (j > 0 && (array[j] < array[j - 1]))
+            {
+                array.Swap(j, j - 1);
+                j = j - 1;
+            }
+
+            i = i + 1;
+        }
+
+        return array;
+    }
+}
