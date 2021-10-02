ce82b21dd0db91226b17ae8710805f93503766bf
Vedant Phougat <vedant.phougat@hotmail.com>
2021-10-02 06:42:32 +0530

    Renamed to List.

--- src/DataStructures/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace VP.DSA.DataStructures;
-
-public class Class1
-{
-    IList<Int32> list = new List<Int32>();
-}
---
+++ src/DataStructures/List.cs
@@ -0,0 +1,23 @@
+﻿namespace VP.DSA.DataStructures;
+
+using System.Collections;
+
+public class List<T> : IList<T>
+{
+    public T this[Int32 index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public Int32 Count => throw new NotImplementedException();
+
+    public Boolean IsReadOnly => throw new NotImplementedException();
+
+    public void Add(T item) => throw new NotImplementedException();
+    public void Clear() => throw new NotImplementedException();
+    public Boolean Contains(T item) => throw new NotImplementedException();
+    public void CopyTo(T[] array, Int32 arrayIndex) => throw new NotImplementedException();
+    public IEnumerator<T> GetEnumerator() => throw new NotImplementedException();
+    public Int32 IndexOf(T item) => throw new NotImplementedException();
+    public void Insert(Int32 index, T item) => throw new NotImplementedException();
+    public Boolean Remove(T item) => throw new NotImplementedException();
+    public void RemoveAt(Int32 index) => throw new NotImplementedException();
+    IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
+}
